277da51a12cde603ed4736e603e7d598fabfdfb2
Hazel Nguyen <hnguyen5@brynmawr.edu>
2024-09-06 15:24:20 -0400

    Assignment 01

--- A01/mathgame.c
+++ A01/mathgame.c
@@ -1,11 +1,38 @@
 /***************************************************
  * mathgame.c
- * Author: 
+ * Author: Hazel Nguyen 
  * Implements a math game
  */
 
 #include <stdio.h>
+#include <stdlib.h> //for rand()
+#include <time.h> //for srand()
 
 int main() {
+  int x; //rounds 
+  int count = 0; 
+  int y; //guess
+
+  printf("%s", "Welcome to Math Game!\nHow many rounds do you want to play? ");
+  scanf("%d", &x);
+
+  //game loop
+  for(int i = 0; i < x; i++) {
+    srand(time(NULL)); //for generating a new time, not deterministic 
+    int value = rand() % (9 + 1);
+    int valueTwo = rand() % (9 + 1);
+    printf("%d%s%d%s", value, " + ", valueTwo, " = ? ");
+    scanf("%d", &y); //guess 
+
+      if (y == (value+valueTwo)) {
+	printf("%s", "\nCorrect!\n");
+	count++;
+      }	
+      else {
+	printf("%s", "\nIncorrect :(\n");
+      }
+  }
+
+  printf("%s%d%s%d%s", "You answered ", count, "/", x, " correctly.\n");
   return 0;
 }
