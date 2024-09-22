9f28b50273e2e2fd33dc39fbce73f17e4bd51cc0
Hazel Nguyen <hnguyen5@brynmawr.edu>
2024-09-21 23:38:53 -0400

    Finished A03

--- A03/dynamic_songs.c
+++ A03/dynamic_songs.c
@@ -1,10 +1,127 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Hazel Nguyen 
+ * Date: 09/20/2024
+ * Description: Reads songs from a text file into an array.
  ---------------------------------------------*/
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct song {
+    char title[128];
+    char artist[128];
+    int durationMin; //parsed from ms
+    int durationSec;
+    float danceability;
+    float energy;
+    float tempo;
+    float valence; //musical positivity level
+} song;
 
 int main() {
-  return 0;
+    FILE *inFile; //cannot be dereferenced!! 
+
+    printf("%s", "Welcome to Hazel's Danceability Directory!\n");
+
+    inFile = fopen("songlist.csv", "r"); //read mode only!
+    if (inFile == NULL) {
+        printf("%s", "Error: Invalid file!\n");
+        return 1; // meaning that the code terminates w/ an error
+    }
+
+    char line[129]; //buffer
+    fgets(line, sizeof(line), inFile); //first line
+    char* token = strtok(line, ",");
+    int len = atoi(token);
+    song *songList = (song*) malloc(sizeof(song) * len); 
+
+    if (songList == NULL) { //checks if valid
+        printf("Space unable to be allocated."); 
+        return 1;
+    }
+    fgets(line, sizeof(line), inFile); //skips header
+
+    int i = 0; //for idx in songList
+    //reading and parsing input
+    while (i != len) {
+        int pos = 0;
+        if (fgets(line, sizeof(line), inFile) != NULL) { //should be at line 3
+            token = strtok(line, ",");
+            while (token != 0) {
+                if (pos == 0) { //put token into struct 
+                    strcpy(songList[i].title, token);
+                }
+
+                if (pos == 1) { //artist
+                    strcpy(songList[i].artist, token);
+                }
+
+                if (pos == 2) { //duration 
+                    int duration = atoi(token);
+                    songList[i].durationMin = (duration/1000)/60;
+                    songList[i].durationSec = (duration/1000)%60;
+                }
+
+                if (pos == 3) { //danceability
+                    songList[i].danceability = atof(token);
+                }
+
+                if (pos == 4) { //energy
+                    songList[i].energy = atof(token);
+                }
+
+                if (pos == 5) { //tempo
+                    songList[i].tempo = atof(token);
+                }
+
+                if (pos == 6) { //valence
+                    songList[i].valence = atof(token);
+                }
+                token = strtok(0, ","); //advances in string
+                pos++;
+            }
+            i++; //next song in songList
+        }
+    } 
+
+    //printing to an attractive table
+    for (int i = 0; i < len; i++) {
+        if (i >= 10) {
+            printf("%d) %-30s artist: %-20s duration: ",
+                    i, songList[i].title,
+                    songList[i].artist);
+        }
+
+        else {
+            printf("%d)  %-30s artist: %-20s duration: ",
+                    i, songList[i].title,
+                    songList[i].artist);
+        }
+
+        if (songList[i].durationSec < 10) { //seconds < 10, needs 0
+            printf("%d:0%d D: %.3f E: %.3f T: %.3f V: %.3f\n",
+                    songList[i].durationMin,
+                    songList[i].durationSec,
+                    songList[i].danceability,
+                    songList[i].energy,
+                    songList[i].tempo,
+                    songList[i].valence);
+        }      
+
+        else { //double digit
+            printf("%d:%d D: %.3f E: %.3f T: %.3f V: %.3f\n",
+                    songList[i].durationMin,
+                    songList[i].durationSec,
+                    songList[i].danceability,
+                    songList[i].energy,
+                    songList[i].tempo,
+                    songList[i].valence);
+
+        }      
+    }
+
+    printf("************************************************\n");
+    free(songList); //always free after malloc!
+    fclose(inFile); //closes the file
+    return 0;
 }
--- A03/slow.c
+++ A03/slow.c
@@ -1,10 +1,47 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Hazel Nguyen 
+ * Date: 09/18/2024
+ * Description: Turns a phrase into Ent speech. 
+ * Asks for pause length & word, outputs Ent
+ * phrase.
  ---------------------------------------------*/
 #include <stdio.h>
+#include <stdlib.h> 
+#include <string.h>
 
 int main() {
-  return 0;
+    int pause; 
+    char word[32]; //in 32 char buffer
+    printf("%s", "Pause length: ");
+    scanf("%d", &pause);
+    printf("%s", "Text: ");
+    scanf("%s", word);
+
+    int word_len = strlen(word); //grabs the length of the word
+    char *ent_word;
+
+    ent_word = malloc(sizeof(char) * (word_len + (pause * word_len)));
+
+    if (ent_word == NULL) { //checks for space!
+        printf("%s", "Not enough space!\n");
+        return 1;
+    }    
+
+    int pos = 0; //for idx in ent_word
+    int i = 0; //for loop, word idx
+    while (word[i] != '\0') {
+        ent_word[pos] = word[i];
+        pos++;
+        for (int x = 0; x < pause; x++) {
+            ent_word[pos] = '.';
+            pos++;
+        }
+        i++;
+    }
+    pos++;
+    ent_word[pos] = '\0'; //terminating char
+    printf("%s\n", ent_word);
+    free(ent_word); //always free after malloc!
+
+    return 0;
 }
