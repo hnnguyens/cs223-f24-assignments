7ed61f512e3bc202eb55d042cbe0fdd313cce358
Hazel Nguyen <hnguyen5@brynmawr.edu>
2024-11-10 21:35:40 -0500

    assignment complete

--- A09/single_mandelbrot.c
+++ A09/single_mandelbrot.c
@@ -1,12 +1,22 @@
+/*----------------------------------------------
+ * Author: Hazel Nguyen
+ * Date: November 10th, 2024
+ * Description: Outputs a PPM image of the 
+ * Mandelbrot set.
+---------------------------------------------*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <stddef.h>
 #include <time.h>
 #include <sys/time.h>
 #include "read_ppm.h"
 #include "write_ppm.h"
 
 int main(int argc, char* argv[]) {
+  struct timeval tstart, tend; //for timer
+  gettimeofday(&tstart, NULL);
+
   int size = 480;
   float xmin = -2.0;
   float xmax = 0.47;
@@ -29,10 +39,63 @@ int main(int argc, char* argv[]) {
   printf("  X range = [%.4f,%.4f]\n", xmin, xmax);
   printf("  Y range = [%.4f,%.4f]\n", ymin, ymax);
 
-  // todo: your work here
-  // generate pallet
-  srand(time(0));
+  // todo: your work here !
+
+  //allocate memory for image
+  struct ppm_pixel *img = malloc((size*size) * sizeof(struct ppm_pixel)); 
+  struct ppm_pixel *palette = malloc((maxIterations) * sizeof(struct ppm_pixel)); 
+  srand(time(0)); //for randomizing the colors 
+
+  //palette (assigning colors)
+  for (int i = 0; i < maxIterations; i++) {
+    palette[i].red = rand() % 255;
+    palette[i].green = rand() % 255;
+    palette[i].blue = rand() % 255;
+  }
+
+  //compute the mandelbrot set; using pseudo code from assignment description
+  for (int r = 0; r < size; r++) {
+    for (int c = 0; c < size; c++) {
+      float xfrac = (float) r / size;
+      float yfrac = (float) c / size;
+      float x0 = xmin + xfrac * (xmax - xmin);
+      float y0 = ymin + yfrac * (ymax - ymin);
+
+      float x = 0;
+      float y = 0;
+      int iter = 0;
+
+      while (iter < maxIterations && x*x + y*y < 2*2) {
+        float xtmp = x*x - y*y + x0;
+        y = 2*x*y + y0;
+        x = xtmp;
+        iter++;
+      }
+
+      if (iter < maxIterations) { 
+        img[c * size + r] = palette[iter];  //write color to image at location (row, col)
+      }
+
+      else {
+        img[c * size + r].red = 0;
+        img[c * size + r].green = 0;
+        img[c * size + r].blue = 0;
+      }
+    }
+  }
 
   // compute image
+  char fileName[64]; //assign name w/ timestamp here 
 
+  //write the image to a PPM file
+  sprintf(fileName, "mandelbrot-%d-%ld.ppm", size, time(0)); //formatting the filename
+  write_ppm(fileName, img, size, size);
+  gettimeofday(&tend, NULL); 
+  float timer = tend.tv_sec - tstart.tv_sec + (tend.tv_usec -tstart.tv_usec)/1.e6;
+
+  printf("Computed mandelbrot set (%d x %d) in %.6f seconds\n", size, size, timer);
+  printf("Writing file: %s\n", fileName);
+
+  free(img);
+  free(palette);
 }
--- A09/write_ppm.c
+++ A09/write_ppm.c
@@ -1,9 +1,51 @@
+/*----------------------------------------------
+ * Author: Hazel Nguyen
+ * Date: October 3rd, 2024
+ * Description: Writes a ppm file. 
+ ---------------------------------------------*/
 #include "write_ppm.h"
 #include <stdio.h>
 #include <string.h>
 
-void write_ppm(const char* filename, struct ppm_pixel* pixels, int w, int h) {
+//Implement *one* (but do not remove the other one)!
+
+/**
+ * Writes a ppm file from a flat array. 
+ * @param filename the file to copy from
+ * @param pixels struct of pixels_ppm
+ * @param w width 
+ * @param h height 
+ * @return void
+*/
+void write_ppm(const char* filename, struct ppm_pixel* pixels, int w, int h) { 
+    FILE *outfile; 
+    outfile = fopen(filename, "w"); //writing into the new file
+
+    if (!outfile) {
+        printf("Error: Unable to open file! %s\n", filename);
+        return;
+    }
+
+    fprintf(outfile, "P6.\n #Created by Hazel Nguyen ver\n%d %d", w, h); //for header
+    //put binary data into a new file; writing it
+    for (int i = 0; i < w; i++) {
+        for (int j = 0; j < h; j++) {
+            unsigned char pixelBlock[3] = {pixels[i * h + j].red, 
+                pixels[i * h + j].green, pixels[i * h + j].blue};
+            fwrite(pixelBlock, sizeof(unsigned char), 3, outfile); //copy
+        }
+    }
+    fclose(outfile); //closes file & prevents memory leak!
 }
 
-void write_ppm_2d(const char* filename, struct ppm_pixel** pxs, int w, int h) {
+/**
+ * Writes a ppm file, reading from a 2D array. 
+ * @param filename the ppm file to copy
+ * @param pixels the 2D array struct 
+ * @param w width 
+ * @param h height 
+ * @return void 
+*/
+void write_ppm_2d(const char* filename, struct ppm_pixel** pixels, int w, int h) { 
+
 }
